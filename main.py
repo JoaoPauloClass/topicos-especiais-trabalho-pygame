eabdf295c5dc3334d8a9b9f8662df07c553fbbf3
JoaoPauloClass <theofficialspam8@gmail.com>
2026-08-19 00:47:01 -0300

    feat: new customization nenu

--- main.py
+++ main.py
@@ -11,8 +11,97 @@ class Menu:
         self.fonte_titulo = pygame.font.SysFont("Arial", 48, bold=True)
         self.fonte_botao = pygame.font.SysFont("Arial", 28)
 
+        self.largura_btn, self.altura_btn = 400,50
+        self.btn_jogar = pygame.Rect(
+            (EngineConfig.WIDTH // 2) - (self.largura_btn // 2),
+            300,
+            self.largura_btn,
+            self.altura_btn,
+        )
+        self.btn_customizar_nave = pygame.Rect(
+            (EngineConfig.WIDTH // 2) - (self.largura_btn // 2),
+            400,
+            self.largura_btn,
+            self.altura_btn,
+        )
+
+        # Cores
+        self.COR_FUNDO = (15, 15, 25)
+        self.COR_TEXTO = (255, 255, 255)
+        self.COR_BOTAO_JOGAR = (0, 150, 200)
+        self.COR_BOTAO_CUSTOMIZAR = (150, 0, 200)
+        self.COR_HOVER_JOGAR = (0, 200, 255) 
+        self.COR_HOVER_CUSTOMIZAR = (200, 0, 255) 
+
+    def processar_eventos(self):
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                if evento.button == 1: 
+                    if self.btn_jogar.collidepoint(evento.pos):
+                        return "JOGANDO"
+                    elif self.btn_customizar_nave.collidepoint(evento.pos):
+                        return "CUSTOMIZANDO"
+
+        return "MENU"
+
+    def desenhar(self):
+        self.tela.fill(self.COR_FUNDO)
+
+        txt_titulo = self.fonte_titulo.render(
+            "SPACE SHOOTER", True, self.COR_TEXTO
+        )
+        rect_titulo = txt_titulo.get_rect(
+            center=(EngineConfig.WIDTH // 2, 180)
+        )
+        self.tela.blit(txt_titulo, rect_titulo)
+
+        pos_mouse = pygame.mouse.get_pos()
+        cor_atual_jogar = (
+            self.COR_HOVER_JOGAR
+            if self.btn_jogar.collidepoint(pos_mouse)
+            else self.COR_BOTAO_JOGAR
+        )
+        
+        cor_atual_customizar = (
+            self.COR_HOVER_CUSTOMIZAR
+            if self.btn_customizar_nave.collidepoint(pos_mouse)
+            else self.COR_BOTAO_CUSTOMIZAR
+        )
+
+        pygame.draw.rect(self.tela, cor_atual_jogar, self.btn_jogar,border_radius=5)
+        pygame.draw.rect(self.tela, cor_atual_customizar, self.btn_customizar_nave,border_radius=5)
+
+        txt_jogar = self.fonte_botao.render("JOGAR", True, self.COR_TEXTO)
+        rect_jogar = txt_jogar.get_rect(center=self.btn_jogar.center)
+        
+        txt_customizar = self.fonte_botao.render("CUSTOMIZAR NAVE", True, self.COR_TEXTO)
+        rect_customizar = txt_customizar.get_rect(
+            center=self.btn_customizar_nave.center
+        )
+        
+        self.tela.blit(txt_jogar, rect_jogar)
+        self.tela.blit(txt_customizar, rect_customizar)
+
+        pygame.display.flip()
+
+class Customizar:
+    def __init__(self, tela):
+        self.tela = tela
+        self.fonte_titulo = pygame.font.SysFont("Arial", 48, bold=True)
+        self.fonte_botao = pygame.font.SysFont("Arial", 28)
+
         self.largura_btn, self.altura_btn = 200,50
         self.btn_jogar = pygame.Rect(
+            (EngineConfig.WIDTH // 2) - (self.largura_btn // 2),
+            300,
+            self.largura_btn,
+            self.altura_btn,
+        )
+        self.btn_customizar_nave = pygame.Rect(
             (EngineConfig.WIDTH // 2) - (self.largura_btn // 2),
             300,
             self.largura_btn,
@@ -22,8 +111,10 @@ class Menu:
         # Cores
         self.COR_FUNDO = (15, 15, 25)
         self.COR_TEXTO = (255, 255, 255)
-        self.COR_BOTAO = (0, 150, 200)
-        self.COR_HOVER = (0, 200, 255)  # Cor quando o mouse passa por cima
+        self.COR_BOTAO_JOGAR = (0, 150, 200)
+        self.COR_BOTAO_CUSTOMIZAR = (150, 0, 200)
+        self.COR_HOVER_JOGAR = (0, 200, 255) 
+        self.COR_HOVER_CUSTOMIZAR = (200, 0, 255) 
 
     def processar_eventos(self):
         for evento in pygame.event.get():
@@ -32,45 +123,52 @@ class Menu:
                 sys.exit()
 
             if evento.type == pygame.MOUSEBUTTONDOWN:
-                if evento.button == 1:  # Botão esquerdo do mouse
-                    # Se clicou no botão "Jogar", retorna True para iniciar o jogo
+                if evento.button == 1: 
                     if self.btn_jogar.collidepoint(evento.pos):
                         return "JOGANDO"
 
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_RETURN:  # Enter também inicia
-                    return "JOGANDO"
-
-        return "MENU"
+        return "CUSTOMIZAR"
 
     def desenhar(self):
         self.tela.fill(self.COR_FUNDO)
 
-        # 1. Desenhar o Título
         txt_titulo = self.fonte_titulo.render(
-            "SPACE SHOOTER", True, self.COR_TEXTO
+            "Customize sua nave", True, self.COR_TEXTO
         )
         rect_titulo = txt_titulo.get_rect(
             center=(EngineConfig.WIDTH // 2, 180)
         )
         self.tela.blit(txt_titulo, rect_titulo)
 
-        # 2. Desenhar o Botão com efeito de iluminação (Hover)
         pos_mouse = pygame.mouse.get_pos()
-        cor_atual = (
-            self.COR_HOVER
+        cor_atual_jogar = (
+            self.COR_HOVER_JOGAR
             if self.btn_jogar.collidepoint(pos_mouse)
-            else self.COR_BOTAO
+            else self.COR_BOTAO_JOGAR
+        )
+        
+        cor_atual_customizar = (
+            self.COR_HOVER_CUSTOMIZAR
+            if self.btn_customizar_nave.collidepoint(pos_mouse)
+            else self.COR_BOTAO_CUSTOMIZAR
         )
 
-        pygame.draw.rect(self.tela, cor_atual, self.btn_jogar, border_radius=10)
+        pygame.draw.rect(self.tela, cor_atual_jogar, self.btn_jogar,border_radius=5)
+        pygame.draw.rect(self.tela, cor_atual_customizar, self.btn_customizar_nave,border_radius=5)
 
-        # 3. Desenhar o Texto do Botão
         txt_jogar = self.fonte_botao.render("JOGAR", True, self.COR_TEXTO)
         rect_jogar = txt_jogar.get_rect(center=self.btn_jogar.center)
+        
+        txt_customizar = self.fonte_botao.render("CUSTOMIZAR NAVE", True, self.COR_TEXTO)
+        rect_customizar = txt_customizar.get_rect(
+            center=(self.btn_jogar.centerx, self.btn_jogar.centery + 120)
+        )
+        
         self.tela.blit(txt_jogar, rect_jogar)
+        self.tela.blit(txt_customizar, rect_customizar)
 
         pygame.display.flip()
+
 
 class Jogo:
     def __init__(self):
@@ -83,6 +181,7 @@ class Jogo:
 
         self.estado = "MENU"
         self.menu = Menu(self.tela)
+        self.customizacao = Customizar(self.tela)
 
         self.clock = pygame.time.Clock()
         self.fps = EngineConfig.FPS
@@ -140,6 +239,10 @@ class Jogo:
                 self.processar_eventos()
                 self.atualizar()
                 self.desenhar()
+                
+            elif self.estado == "CUSTOMIZANDO":
+                self.estado = self.customizacao.processar_eventos()
+                self.customizacao.desenhar()
 
 
 if __name__ == "__main__":
